224005fb1bbd728928781b3479e643f972b5863e
Jonas Scharpf <jonas@brainelectronics.de>
2022-02-06 15:49:39 +0100

    fix missing return value of PathHelper exists function of simulation

--- simulation/src/path_helper/path_helper.py
+++ simulation/src/path_helper/path_helper.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""
-Provide unavailable path functions in MicroPython
-"""
+"""Provide unavailable path functions in MicroPython"""
 
+# import os
 from pathlib import Path
 
 
@@ -26,3 +25,44 @@ class PathHelper(object):
         :rtype:     bool
         """
         result = Path(path).exists()
+        return result
+
+        """
+        result = False
+
+        path_to_file_list = path.split('/')
+        # ['path', 'to', 'some', 'file.txt']
+
+        root_path = ''
+        # if sys.platform == 'esp32':
+        # if sys.platform not in ['aix', 'linux', 'win32', 'cygwin', 'darwin']:
+        #     root_path = ''
+        # else:
+        #     root_path = os.path.dirname(os.path.abspath(__file__))
+        # print('The root path: {}'.format(root_path))
+
+        this_path = root_path
+        for ele in path_to_file_list[:-1]:
+            files_in_dir = os.listdir(this_path)
+            # print('Files in {}: {}'.format(this_path, files_in_dir))
+
+            if ele in files_in_dir:
+                # print('"{}" found in "{}"'.format(ele, files_in_dir))
+
+                if this_path == '':
+                    this_path += '{}'.format(ele)
+                else:
+                    this_path += '/{}'.format(ele)
+
+                # print('Next folder to be checked is: {}'.format(this_path))
+            else:
+                return result
+
+        files_in_dir = os.listdir(this_path)
+        if path_to_file_list[-1] in files_in_dir:
+            # print('File "{}" found in "{}"'.
+            #       format(path_to_file_list[-1], this_path))
+            return True
+        else:
+            return False
+        """
