cbaf943214b3e22256f3ced001010c8f54aab813
Jonas Scharpf <jonas@brainelectronics.de>
2022-02-25 12:44:21 +0100

    return to REPL from main if connected, update imports

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ main script, do your stuff here, similar to the loop() function on Arduino
 
 from wifi_manager import WiFiManager
 
-if __name__ == '__main__':
-    wm = WiFiManager()
-    result = wm.load_and_connect()
-    wm.start_config()
+wm = WiFiManager()
 
-    # if result is False:
-    #     wm.start_config()
-    # else:
-    #     print('Successfully connected to a network :)')
+# if there is enough RAM on the board, may increase the buffer size on send
+# stream operations to read bigger chunks from disk, default is 128
+# wm.app.SEND_BUFSZ = 1024
+
+result = wm.load_and_connect()
+print('Connection result: {}'.format(result))
+
+if result is False:
+    print('Starting config server')
+    wm.start_config()
+else:
+    print('Successfully connected to a network :)')
+
+print('Returning to REPL')
