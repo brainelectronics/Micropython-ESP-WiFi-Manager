ac9035d8f103c91d9ff05ef4c6f6a57705461fd7
Jonas Scharpf <jonas@brainelectronics.de>
2022-11-04 09:09:47 +0100

    increase coverage of fake RTC to 100%, see #16

--- simulation/tests/test_rtc.py
+++ simulation/tests/test_rtc.py
@@ -3,7 +3,9 @@
 
 """Unittest of Machine RTC"""
 
+from datetime import datetime
 import unittest
+import time
 
 # custom imports
 from machine import RTC
@@ -11,10 +13,40 @@ from machine import RTC
 
 class TestRTC(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.rtc = RTC()
 
     def tearDown(self) -> None:
         pass
+
+    def test__init(self) -> None:
+        """Test initial values"""
+        time_tuple = self.rtc._time_tuple
+        self.assertIsInstance(time_tuple, tuple)
+        self.assertTrue(all(isinstance(ele, int) for ele in time_tuple))
+        self.assertEqual(time_tuple, (2000, 1, 1, 0, 0, 0, 0, 0))
+
+    def test_init(self) -> None:
+        """Test initialization of RTC"""
+        timezone = 0
+        tm = time.localtime()
+        now = datetime.fromtimestamp(time.mktime(tm))
+
+        self.rtc.init(
+            (tm[0], tm[1], tm[2], tm[3], tm[4], tm[5], 0, timezone)
+        )
+
+        time_tuple = self.rtc._time_tuple
+        self.assertEqual(time_tuple,
+                         (now.year, now.month, now.day,
+                          now.weekday(),
+                          now.hour, now.minute, now.second, 0))
+
+    def test_datetime(self) -> None:
+        """Test getting current datetime"""
+        datetime = self.rtc.datetime()
+        self.assertIsInstance(datetime, tuple)
+        self.assertTrue(all(isinstance(ele, int) for ele in datetime))
+        self.assertEqual(datetime, (2000, 1, 1, 0, 0, 0, 0, 0))
 
 
 if __name__ == '__main__':
