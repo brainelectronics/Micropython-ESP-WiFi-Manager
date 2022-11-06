3c3bf8b9889494eda0b4d04855710f967c0b14d0
Jonas Scharpf <jonas@brainelectronics.de>
2022-11-06 09:52:23 +0100

    increase led_helper coverage above 70%

--- simulation/tests/test_led_helper.py
+++ simulation/tests/test_led_helper.py
@@ -4,9 +4,11 @@
 """Unittest of LED Helper"""
 
 from datetime import datetime
+from io import StringIO
 import math
 from nose2.tools import params
 from random import randint, randrange
+import _thread
 import time
 import unittest
 from unittest.mock import patch
@@ -15,6 +17,7 @@ from unittest.mock import patch
 from led_helper import Led
 from led_helper import Neopixel
 from led_helper import NeoPixel
+from machine import Pin
 
 
 class TestLed(unittest.TestCase):
@@ -27,8 +30,7 @@ class TestLed(unittest.TestCase):
 
     def test__init__(self) -> None:
         """Test initial values"""
-        # check thread specific values
-        # self.assertIsInstance(self.led._fade_lock, _thread.allocate_lock)
+        self.assertIsInstance(self.led.led_pin, Pin)
         self.assertEqual(self.led._inverted, False)
         self.assertEqual(self.led._blink_delay, 250)
 
@@ -66,6 +68,7 @@ class TestLed(unittest.TestCase):
 
         # it is a static method
         self.led.blink(delay_ms=delay_ms)
+        time.sleep(0.1)     # thread might not start immediately
         self.assertTrue(self.led.blinking)
         time.sleep(blink_time)
 
@@ -76,15 +79,49 @@ class TestLed(unittest.TestCase):
         # 1. get current LED pin state
         # 2. set new inverted LED pin state
         expected_call_count = math.ceil((blink_time * 1000) / delay_ms) * 2
-        print('delay_ms: {}'.format(delay_ms))
-        print('expected_call_count: {}'.format(expected_call_count))
+        self.assertGreaterEqual(mocked_pin_value.call_count,
+                                expected_call_count)
 
-        self.assertEqual(mocked_pin_value.call_count, expected_call_count)
-
-    @unittest.skip("Checked by test_blink")
-    def test__blink(self) -> None:
+    @patch('machine.pin.Pin.value')
+    def test__blink(self, mocked_pin_value: unittest.mock.MagicMock) -> None:
         """Test internal blink thread content"""
-        pass
+        delay_ms = randrange(10, 100, 10)
+        blink_time = 1.0    # second
+        blink_lock = _thread.allocate_lock()
+
+        blink_lock.acquire()
+        params = (delay_ms, blink_lock)
+        _thread.start_new_thread(self.led._blink, params)
+
+        time.sleep(0.1)     # thread might not start immediately
+        self.assertTrue(blink_lock.locked())
+        time.sleep(blink_time)
+
+        # terminate thread
+        blink_lock.release()
+        time.sleep(0.1)
+        self.assertFalse(blink_lock.locked())
+
+        # expected call of pin.value() function is two times of blink amount
+        # 1. get current LED pin state
+        # 2. set new inverted LED pin state
+        # +1 due to "turn_off" call after leaving the while loop
+        expected_call_count = math.ceil((blink_time * 1000) / delay_ms) * 2 + 1
+        self.assertGreaterEqual(mocked_pin_value.call_count,
+                                expected_call_count)
+
+        # start thread without acquired locking
+        _thread.start_new_thread(self.led._blink, params)
+
+        time.sleep(blink_time)
+
+        self.assertFalse(blink_lock.locked())
+
+        # expected call of pin.value() function is one
+        # 1. due to "turn_off" call after leaving the while loop
+        expected_call_count = 1
+        self.assertGreaterEqual(mocked_pin_value.call_count,
+                                expected_call_count)
 
     @params(
         (-10),
@@ -202,7 +239,7 @@ class TestLed(unittest.TestCase):
 
 class TestNeopixel(unittest.TestCase):
     def setUp(self) -> None:
-        self.pixel = Neopixel()
+        self.pixel = Neopixel(neopixel_pin=27, neopixels=1)
 
     def tearDown(self) -> None:
         pass
@@ -211,6 +248,25 @@ class TestNeopixel(unittest.TestCase):
         """Test initial values"""
         self.assertEqual(self.pixel._neopixel_amount, 1)
         self.assertIsInstance(self.pixel.pixel, NeoPixel)
+
+        self.assertEqual(
+            self.pixel._colors,
+            {
+                'red': [30, 0, 0],
+                'green': [0, 30, 0],
+                'blue': [0, 0, 30],
+                'yellow': [30, 30, 0],
+                'cyan': [0, 30, 30],
+                'magenta': [30, 0, 30],
+                'white': [30, 30, 30],
+                'maroon': [30 // 2, 0, 0],
+                'darkgreen': [0, 30 // 2, 0],
+                'darkblue': [0, 0, 30 // 2],
+                'olive': [30 // 2, 30 // 2, 0],
+                'teal': [0, 30 // 2, 30 // 2],
+                'purple': [30 // 2, 0, 30 // 2],
+            }
+        )
 
         # check values of the pixel
         self.assertEqual(self.pixel._color, [30, 0, 0])
@@ -224,55 +280,153 @@ class TestNeopixel(unittest.TestCase):
         self.assertEqual(self.pixel._fading, False)
         self.assertEqual(self.pixel._fade_pixel_amount, -1)
 
-    @unittest.skip("Not yet implemented")
-    def test_clear(self) -> None:
+    @patch('led_helper.led_helper.Neopixel.set')
+    def test_clear(self, mocked_neopixel_set: unittest.mock.MagicMock) -> None:
         """Test clearing Neopixel color"""
-        pass
+        self.pixel.clear(number=3)
+        mocked_neopixel_set.assert_called_once_with(rgb=[0, 0, 0], number=3)
 
     @unittest.skip("Not yet implemented")
     def test_set(self) -> None:
         """Test setting Neopixel color"""
         pass
 
-    @unittest.skip("Not yet implemented")
-    def test_red(self) -> None:
+    @patch('led_helper.led_helper.Neopixel.set')
+    def test_red(self, mocked_neopixel_set: unittest.mock.MagicMock) -> None:
         """Test setting Neopixel color to red"""
-        pass
+        self.pixel.green(intensity=40, number=2)
+        mocked_neopixel_set.assert_called_once_with(green=40, number=2)
 
-    @unittest.skip("Not yet implemented")
-    def test_green(self) -> None:
+    @patch('led_helper.led_helper.Neopixel.set')
+    def test_green(self, mocked_neopixel_set: unittest.mock.MagicMock) -> None:
         """Test setting Neopixel color to green"""
-        pass
+        self.pixel.green(intensity=50, number=3)
+        mocked_neopixel_set.assert_called_once_with(green=50, number=3)
 
-    @unittest.skip("Not yet implemented")
-    def test_blue(self) -> None:
+    @patch('led_helper.led_helper.Neopixel.set')
+    def test_blue(self, mocked_neopixel_set: unittest.mock.MagicMock) -> None:
         """Test setting Neopixel color to blue"""
-        pass
+        self.pixel.blue(intensity=60, number=4)
+        mocked_neopixel_set.assert_called_once_with(blue=60, number=4)
 
-    @unittest.skip("Not yet implemented")
     def test_pixels(self) -> None:
         """Test getting number of Neopixels"""
-        pass
+        self.assertEqual(self.pixel.pixels, 1)
 
-    @unittest.skip("Not yet implemented")
-    def test_color(self) -> None:
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('led_helper.led_helper.Neopixel.clear')
+    def test_color(self,
+                   mocked_clear: unittest.mock.MagicMock,
+                   mock_stdout: StringIO) -> None:
         """Test getting and setting the current Neopixel color"""
-        pass
+        self.assertEqual(self.pixel.color, [30, 0, 0])
 
-    @unittest.skip("Not yet implemented")
+        # set color as string value
+        self.pixel.color = 'blue'
+        self.assertEqual(self.pixel.color, [0, 0, 30])
+
+        # set undefined color by string value
+        undefined_color = 'grey'
+        self.pixel.color = undefined_color
+        self.assertIn('Color "{}" unknown, may add it with "colors" function'.
+                      format(undefined_color),
+                      mock_stdout.getvalue())
+        # color should not be changed
+        self.assertEqual(self.pixel.color, [0, 0, 30])
+
+        # set color as list of int
+        self.pixel.color = [0, 30, 0]
+        self.assertEqual(self.pixel.color, [0, 30, 0])
+
+        # turn Neopixel off
+        self.pixel.color = [0, 0, 0]
+        self.assertEqual(self.pixel.color, [0, 0, 0])
+        self.assertEqual(mocked_clear.call_count, 1)
+
     def test_intensity(self) -> None:
         """Test getting and setting the current Neopixel intensity"""
-        pass
+        self.pixel.active = True    # pixel must be active to change intensity
+        self.assertTrue(self.pixel.active)
+        self.assertEqual(self.pixel.color, [30, 0, 0])
 
-    @unittest.skip("Not yet implemented")
-    def test_active(self) -> None:
+        new_intensity = 100
+
+        with patch('led_helper.led_helper.Neopixel.set') as mocked_set:
+            self.pixel.intensity = new_intensity
+            self.assertEqual(self.pixel._last_intensity, new_intensity)
+            mocked_set.assert_called_once_with(rgb=[100, 0, 0], number=-1)
+
+        # test turning pixel off via intensity settings
+        new_intensity = 0
+
+        with patch('led_helper.led_helper.Neopixel.clear') as mocked_clear:
+            self.pixel.intensity = new_intensity
+            self.assertEqual(self.pixel._last_intensity, new_intensity)
+            mocked_clear.assert_called_once_with(number=-1)
+
+    @patch('led_helper.led_helper.Neopixel.set')
+    @patch('led_helper.led_helper.Neopixel.clear')
+    def test_active(self,
+                    mocked_clear: unittest.mock.MagicMock,
+                    mocked_set: unittest.mock.MagicMock) -> None:
         """Test getting and setting the current Neopixel status"""
-        pass
+        current_pixel_state = self.pixel.active
+        current_pixel_color = self.pixel.color
+        self.assertFalse(current_pixel_state)
 
-    @unittest.skip("Not yet implemented")
+        # disable pixel again
+        self.pixel.active = False
+        self.assertFalse(self.pixel.active)
+
+        # enable pixel
+        self.pixel.active = True
+        self.assertTrue(self.pixel.active)
+        mocked_set.assert_called_once_with(rgb=current_pixel_color, number=-1)
+
+        # disable pixel
+        self.pixel.active = False
+        self.assertFalse(self.pixel.active)
+        mocked_clear.assert_called_once_with(number=-1)
+
     def test_colors(self) -> None:
         """Test getting and setting the available Neopixel colors"""
-        pass
+        default_colors = {
+            'red': [30, 0, 0],
+            'green': [0, 30, 0],
+            'blue': [0, 0, 30],
+            'yellow': [30, 30, 0],
+            'cyan': [0, 30, 30],
+            'magenta': [30, 0, 30],
+            'white': [30, 30, 30],
+            'maroon': [30 // 2, 0, 0],
+            'darkgreen': [0, 30 // 2, 0],
+            'darkblue': [0, 0, 30 // 2],
+            'olive': [30 // 2, 30 // 2, 0],
+            'teal': [0, 30 // 2, 30 // 2],
+            'purple': [30 // 2, 0, 30 // 2],
+        }
+        default_colors_number = 13
+        self.assertIsInstance(self.pixel.colors, dict)
+        self.assertEqual(len(self.pixel.colors), default_colors_number)
+        self.assertEqual(self.pixel.colors, default_colors)
+
+        # update colors with already existing color
+        updated_color = {'red': [10, 0, 0]}
+        default_colors.update(updated_color)
+        self.pixel.colors = updated_color
+
+        self.assertIsInstance(self.pixel.colors, dict)
+        self.assertEqual(len(self.pixel.colors), default_colors_number)
+        self.assertEqual(self.pixel.colors, default_colors)
+
+        # add new color
+        new_color = {'black': [0, 0, 0]}
+        default_colors.update(new_color)
+        self.pixel.colors = new_color
+
+        self.assertIsInstance(self.pixel.colors, dict)
+        self.assertEqual(len(self.pixel.colors), default_colors_number + 1)
+        self.assertEqual(self.pixel.colors, default_colors)
 
     @unittest.skip("Not yet implemented")
     def test_fade(self) -> None:
@@ -284,10 +438,32 @@ class TestNeopixel(unittest.TestCase):
         """Test fade thread"""
         pass
 
-    @unittest.skip("Not yet implemented")
-    def test_fade_delay(self) -> None:
-        """Test getting and setting the fade delay"""
+    @params(
+        (-10),
+        (0),
+        (1),
+        (123),
+    )
+    def test_fade_delay(self, value: int) -> None:
+        """
+        Test getting and setting the fade delay
+
+        :param      value:  The value
+        :type       value:  int
+        """
         pass
+
+        default_fade_delay = self.pixel.fade_delay
+        self.assertEqual(default_fade_delay, 50)
+
+        # set and get new blink delay
+        self.pixel.fade_delay = value
+        fade_delay = self.pixel.fade_delay
+
+        if value > 1:
+            self.assertEqual(fade_delay, value)
+        else:
+            self.assertEqual(fade_delay, 1)
 
     @unittest.skip("Not yet implemented")
     def test_fade_pixel_amount(self) -> None:
