d4f275355ba8d63d7860a1c2feb97ba2feaa2505
Jonas Scharpf <jonas@brainelectronics.de>
2023-06-12 09:17:34 +0200

    remove device specific dependencies from boot and main files

--- boot.py
+++ boot.py
@@ -5,36 +5,20 @@
 boot script, do initial stuff here, similar to the setup() function on Arduino
 """
 
-import esp
 import gc
 import network
-import time
+from time import sleep
 
-# custom packages
-from be_helpers.led_helper import Led
-
-
-# set clock speed to 240MHz instead of default 160MHz
-# machine.freq(240000000)
-
-# disable ESP os debug output
-esp.osdebug(None)
-
-led = Led()
-led.flash(amount=3, delay_ms=50)
-led.turn_on()
 
 station = network.WLAN(network.STA_IF)
 if station.active() and station.isconnected():
     station.disconnect()
-    time.sleep(1)
+    sleep(1)
 station.active(False)
-time.sleep(1)
+sleep(1)
 station.active(True)
-
-led.turn_off()
 
 # run garbage collector at the end to clean up
 gc.collect()
 
-print('Finished booting')
+print('Finished booting steps of MicroPython WiFiManager')
